d0bd6f0945c331904188d927b46ca7c0ad5ccd8d
Žygimantas Mickavičius <zygimantasmickavicius@gmail.com>
2025-02-01 22:38:27 +0100

    adjustment

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,114 +6,86 @@ from datetime import datetime
 import altair as alt
 import pydeck as pdk
 
+st.set_page_config(layout="wide")
 
 st.header('Data Analysis of AB "Siauliu Energija"')
 
 sidebar = st.sidebar
 sidebar.header("Settings")
-
+res_side = sidebar.selectbox("Select category", ("Heat, kWh", "Hot water, m³"), placeholder="Heat", key=47)
 tab1, tab2, tab3, tab4 = st.tabs(["Overall Consumption Trends", "Trends by building function", "Rooms data", "Geospatial Data"])
 with tab1:
-    #_---------------------------------------------
-    # Monthly trend data
-    monthly_trend = pd.read_csv("monthly_trend.csv")
-    monthly_trend["Date"] = pd.to_datetime(monthly_trend["month"])
-    st.write("### Monthly Energy Consumption Trends")
-    res = st.selectbox("Select category", ("Heat, kWh", "Hot water, m³"), placeholder="Heat")
-    opt = {
-        'Heat, kWh': 'Šiluma',
-        'Hot water, m³': 'Karštas vanduo'
-    }
-    res_lt = opt[res]
-    st.line_chart(monthly_trend, x="Date", y=res_lt, y_label=res)
-    #_---------------------------------------------
-    # Yearly trend data
-    monthly_trend = pd.read_csv("yearly_trend.csv")
-    st.write("### Yearly Energy Consumption Trends")
-    res = st.selectbox("Select category", ("Heat, kWh", "Hot water, m³"), placeholder="Heat", key=42)
-    opt = {
-        'Heat, kWh': 'Šiluma',
-        'Hot water, m³': 'Karštas vanduo'
-    }
-    res_lt = opt[res]
-    st.bar_chart(monthly_trend, x="year", y=res_lt, y_label=res)
-    #------------------------------------------------
-    # Contracts monthly trend
-    contract_trend = pd.read_csv("contract_trend.csv")
-    st.write("### Monthly trend of Number Of Provided Services")
-    st.line_chart(contract_trend, x="month", y="0")
-    #------------------------------------------------
-    # Contracts yearly trend
-    contract_trend = pd.read_csv("contract_trend_yearly.csv")
-    st.write("### Yearly trend of Number Of Provided Services")
-    st.bar_chart(contract_trend, x="year", y="0")
-    #----------------------------------------------
-with tab2:
-    # Building funcs
-    rooms = pd.read_csv("rooms.csv")
-    func_val_counts = rooms["building_func"].value_counts()
-    st.table(func_val_counts)
-    #------------------------------------------------
-    # Yearly consumption by building func
-    heat_cons_by_func = pd.read_csv("heat_cons_by_func.csv")
-    wat_cons_by_func = pd.read_csv("wat_cons_by_func.csv")
-    st.write("### Yearly Heat Consumption trend by building function")
-
-    res = st.selectbox("Select category", ("Heat, kWh", "Hot water, m³"), placeholder="Heat", key=43)
-    opt = {
-        'Heat, kWh': heat_cons_by_func,
-        'Hot water, m³': wat_cons_by_func
-    }
-    res_lt = opt[res]
-
-    df_melted = res_lt.melt(id_vars=['year'], var_name='Category', value_name='Value')
-
-    chart = alt.Chart(df_melted).mark_bar().encode(
-        x=alt.X('year:O', title='Year', scale=alt.Scale(zero=False)),
-        y=alt.Y('sum(Value):Q', title='Total Value', scale=alt.Scale(zero=False)),
-        color='Category:N',
-        order=alt.Order('Value', sort="descending")
-    ).properties(
-        width=600,
-        height=400).interactive()
-    st.altair_chart(chart, use_container_width=True)
-
-    st.header("Average monthly heat consumption by building function")
-    func_df = pd.read_csv("func_df.csv")
-    bar_chart = alt.Chart(func_df).mark_bar().encode(
-    y=alt.Y('building_func:N', sort='-x', title='Building Function'),
-    x=alt.X('eff:Q', title='Average Monthly Heat Consumption')
-    ).properties(
-    title='Average Monthly Heat Consumption by Building Function'
+    st.markdown(
+    "<h1 style='text-align: center;'>Energy Consumption Trends</h1>",
+    unsafe_allow_html=True
     )
-    st.altair_chart(bar_chart, use_container_width=True)
-with tab3:
-    #-------------------------------------------------
-    # rooms per buyer
-    st.write("### Number of rooms per buyer, top 20")
-    buyer_rooms = pd.read_csv("buyer_rooms.csv")
-    st.bar_chart(buyer_rooms["room_id"])
-
-    #-------------------------------------------
-    # Consumption by building year
-
-    heat_by_build_year = pd.read_csv("heat_by_build_year.csv")
-    st.header("Heat consumption per square meter by building build year")
-    st.write("Over time we can see that heat consumption decreases as buildings get younger, which can be attributed to better technology, heat insulation maybe")
-    st.bar_chart(heat_by_build_year, x="build_year", y="eff")
-    #------------------------------------------------------------------
-    area_df = pd.read_csv("area_df.csv")
-    st.header("Average heat consumption per square meter against area size")
-
-    st.bar_chart(area_df, x="area_bins", y="eff")
-with tab4:
-    st.header("Geospatial consumption heatmap")
-    res = st.selectbox("Select category", ("Heat, kWh", "Hot water, m³"), placeholder="Heat", key=44)
+    col1, col2 = st.columns(2)
+    
+    
+    with col1:
+        #_---------------------------------------------
+        # Monthly trend data
+        st.markdown(
+            "<h3 style='text-align: center;'>Monthly</h1>",
+            unsafe_allow_html=True
+        )
+        monthly_trend = pd.read_csv("monthly_trend.csv")
+        monthly_trend["Date"] = pd.to_datetime(monthly_trend["month"])
+        
+        opt = {
+            'Heat, kWh': 'Šiluma',
+            'Hot water, m³': 'Karštas vanduo'
+        }
+        res_lt = opt[res_side]
+        st.line_chart(monthly_trend, x="Date", y=res_lt, y_label=res_side)
+        
+    with col2:
+        st.markdown(
+            "<h3 style='text-align: center;'>Yearly</h1>",
+            unsafe_allow_html=True
+        )
+        #_---------------------------------------------
+        # Yearly trend data
+        monthly_trend = pd.read_csv("yearly_trend.csv")
+        opt = {
+            'Heat, kWh': 'Šiluma',
+            'Hot water, m³': 'Karštas vanduo'
+        }
+        res_lt = opt[res_side]
+        st.bar_chart(monthly_trend, x="year", y=res_lt, y_label=res_side)
+        #------------------------------------------------
+    st.markdown(
+    "<h1 style='text-align: center;'>Number Of Provided Services Trends</h1>",
+    unsafe_allow_html=True
+    )
+    col3, col4 = st.columns(2)
+    with col3:
+        # Contracts monthly trend
+        contract_trend = pd.read_csv("contract_trend.csv")
+        st.markdown(
+            "<h3 style='text-align: center;'>Monthly</h1>",
+            unsafe_allow_html=True
+        )
+        st.line_chart(contract_trend, x="month", y="0")
+    with col4:
+        #------------------------------------------------
+        # Contracts yearly trend
+        contract_trend = pd.read_csv("contract_trend_yearly.csv")
+        st.markdown(
+            "<h3 style='text-align: center;'>Yearly</h1>",
+            unsafe_allow_html=True
+        )
+        st.bar_chart(contract_trend, x="year", y="0")
+        #----------------------------------------------
+    st.markdown(
+    "<h1 style='text-align: center;'>Consumption Heatmap</h1>",
+    unsafe_allow_html=True
+    )
     opt = {
         'Heat, kWh': "geo_amount_heat.csv",
         'Hot water, m³': "geo_amount_wat.csv"
     }
-    res_lt = opt[res]
+    res_lt = opt[res_side]
 
     date_range = pd.date_range(start="2019-01-01", end="2023-12-31", freq='MS').to_pydatetime().tolist()
 
@@ -122,7 +94,6 @@ with tab4:
     res_date = str(res_date.date())
     geo_amount = pd.read_csv(res_lt)
     geo_amount_filtered = geo_amount[geo_amount["month"] == res_date]
-    st.write(len(geo_amount_filtered))
     st.pydeck_chart(
     pdk.Deck(
         map_style=None,
@@ -147,6 +118,81 @@ with tab4:
     )
     
     )
+    
+with tab2:
+    
+    col5, col6 = st.columns(2)
+    # Building funcs
+    with col5:
+        st.markdown(
+            "<h3 style='text-align: center;'>Number of rooms by building function</h1>",
+            unsafe_allow_html=True
+        )
+        rooms = pd.read_csv("rooms.csv")
+        func_val_counts = rooms["building_func"].value_counts()
+        st.table(func_val_counts)
+    with col6:
+        st.markdown(
+            "<h3 style='text-align: center;'>Consumption Efficiency by function</h1>",
+            unsafe_allow_html=True
+        )
+        func_df = pd.read_csv("func_df.csv")
+        bar_chart = alt.Chart(func_df).mark_bar().encode(
+        y=alt.Y('building_func:N', sort='-x', title='Building Function'),
+        x=alt.X('eff:Q')
+        ).properties(height=635
+        )
+        st.altair_chart(bar_chart, use_container_width=True)
+    #------------------------------------------------
+    # Yearly consumption by building func
+    heat_cons_by_func = pd.read_csv("heat_cons_by_func.csv")
+    wat_cons_by_func = pd.read_csv("wat_cons_by_func.csv")
+    st.write("### Yearly Consumption trend by building function")
+
+    opt = {
+        'Heat, kWh': heat_cons_by_func,
+        'Hot water, m³': wat_cons_by_func
+    }
+    res_lt = opt[res_side]
+
+    df_melted = res_lt.melt(id_vars=['year'], var_name='Category', value_name='Value')
+
+    chart = alt.Chart(df_melted).mark_bar().encode(
+        x=alt.X('year:O', title='Year', scale=alt.Scale(zero=False)),
+        y=alt.Y('sum(Value):Q', title='Total Value', scale=alt.Scale(zero=False)),
+        color='Category:N',
+        order=alt.Order('Value', sort="descending")
+    ).properties(
+        width=600,
+        height=400).interactive()
+    st.altair_chart(chart, use_container_width=True)
+
+    
+with tab3:
+    #-------------------------------------------------
+    # rooms per buyer
+    st.write("### Number of rooms per buyer, top 20")
+    buyer_rooms = pd.read_csv("buyer_rooms.csv")
+    st.bar_chart(buyer_rooms["room_id"])
+
+    #-------------------------------------------
+    # Consumption by building year
+
+    heat_by_build_year = pd.read_csv("heat_by_build_year.csv")
+    st.header("Heat consumption per square meter by building build year")
+    st.write("Over time we can see that heat consumption decreases as buildings get younger, which can be attributed to better technology, heat insulation maybe")
+    st.bar_chart(heat_by_build_year, x="build_year", y="eff")
+    #------------------------------------------------------------------
+    area_df = pd.read_csv("area_df.csv")
+    st.header("Average heat consumption per square meter against area size")
+
+    chart = alt.Chart(area_df).mark_bar().encode(
+        x=alt.X("area_bins:N", sort="-y"),
+        y="eff:Q",
+    )
+    st.altair_chart(chart, use_container_width=True)
+with tab4:
+    
     st.header("Geospatial building age heatmap")
 
     geo_build = pd.read_csv("geo_build.csv")
